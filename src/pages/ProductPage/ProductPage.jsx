dcb09734d78a2c2c6631dfc04264907570be821c
Oleg <sleite4r@gmail.com>
2025-06-05 11:34:23 +0200

    Implement filtering, sorting (incl. rating), favorites, and pagination

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import ProductList from "../../components/ProductList/ProductList";
+import Pagination from "../../components/Pagination/Pagination";
+import FilterPanel from "../../components/FilterPanel/FilterPanel";
 
 import {
   getFavoritesFromStorage,
@@ -10,9 +13,18 @@ const ProductPage = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const [favoriteIds, setFavoriteIds] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [sortOption, setSortOption] = useState("");
+  const [showOnlyFavorites, setShowOnlyFavorites] = useState(false);
+
   //Favorite with localStorage
-  const [favoriteId, setFavoriteId] = useState("getFavoritesFromStorage");
+  const [favoriteIds, setFavoriteId] = useState(getFavoritesFromStorage);
+
+  //Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsPerPage = 10;
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -34,9 +46,94 @@ const ProductPage = () => {
     fetchProduct();
   }, []);
 
+  useEffect(() => {
+    saveFavoritesToStorage(favoriteIds);
+  }, [favoriteIds]);
+
+  const categories = [...new Set(products.map((p) => p.category))];
+
+  function resetFilters() {
+    setSearchTerm("");
+    setSelectedCategory("");
+    setSortOption("");
+    setShowOnlyFavorites("");
+    setCurrentPage(1);
+  }
+
+  function toggleFavorite(id) {
+    setFavoriteId((favIds) =>
+      favIds.includes(id) ? favIds.filter((fid) => fid !== id) : [...favIds, id]
+    );
+  }
+
+  const filteredProducts = products
+    .filter((p) => p.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter((p) => (selectedCategory ? p.category === selectedCategory : true))
+    .filter((p) => (showOnlyFavorites ? favoriteIds.includes(p.id) : true))
+    .sort((a, b) => {
+      switch (sortOption) {
+        case "priceAsc":
+          return a.price - b.price;
+        case "priceDesc":
+          return b.price - a.price;
+        case "alphaAsc":
+          return a.title.localeCompare(b.title);
+        case "alphaDesc":
+          return b.title.localeCompare(a.title);
+        case "ratingDesc":
+          return b.rating - a.rating;
+        default:
+          return 0;
+      }
+    });
+
+  const totalPges = Match.ceil(filteredProducts.length / itemsPerPage);
+  const pageProducts = filteredProducts.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  useEffect(() => {
+    if (currentPage > totalPges) {
+      setCurrentPage(1);
+    }
+  }, [filteredProducts, currentPage, totalPges]);
+
   return (
     <div>
-      <h1>ProductPage</h1>
+      <div>
+        <h2>Shop</h2>
+      </div>
+
+      <FilterPanel
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        categories={categories}
+        setSortOption={setSortOption}
+        showOnlyFavorites={showOnlyFavorites}
+        setShowOnlyFavorites={setShowOnlyFavorites}
+        resetFilters={resetFilters}
+      />
+
+      {loading && <p>Loader produkter...</p>}
+      {error && <p className={s.error}>Fejl: {error}</p>}
+      {!loading && !error && (
+        <>
+          <ProductList
+            products={pageProducts}
+            favoriteIds={favoriteIds}
+            toggleFavorite={toggleFavorite}
+          />
+
+          <Pagination
+            currentPage={currentPage}
+            totalPges={totalPges}
+            setCurrentPage={setCurrentPage}
+          />
+        </>
+      )}
     </div>
   );
 };
